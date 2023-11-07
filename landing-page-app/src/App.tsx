4a1fc9e4cccb27e7e45ad5f0dabffcefc464f221
Sylu <mortisflos@gmail.com>
2023-11-07 17:24:31 +0500

    init

---
+++ landing-page-app/src/App.tsx
@@ -0,0 +1,23 @@
+import "./styles/App.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <section className="main-screen">
+        <div className="main-screen-back">
+          <div className="img-neuron"></div>
+          <div className="circle-1 circle"></div>
+          <div className="circle-1 circle"></div>
+          <div className="circle-1 circle"></div>
+          <div className="circle-1 circle"></div>
+        </div>
+        <header>
+          <div className="logo-block"></div>
+          <div className="phone-block"></div>
+        </header>
+      </section>
+    </div>
+  );
+}
+
+export default App;
